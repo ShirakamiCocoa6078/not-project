975e597094b9e8ea4cdaa7e7c9d7624b6fafba9d
Hotococoa6078 <71052778+Hotococoa6078@users.noreply.github.com>
2021-03-13 13:52:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,18 +10,55 @@ function characterChanged() {
 }
 
 function changeWeapon(){
-    const options1 = document.getElementById('weaponLevel')
-    const options2 = document.getElementById('weaponBreak')
+    const options = document.getElementById('weaponLevel')
     document.getElementById('weaponLevel').selectedIndex = 0
-    document.getElementById('weaponBreak').selectedIndex = 0
 
     const weaponElement = document.getElementById('weapon')
     const selectedClass = weaponElement.options[weaponElement.selectedIndex].value
-    for(let i = 1; i< options1.length; i++){
+    for(let i = 1; i< options.length; i++){
         if(selectedClass[-1].match(/1|2/)){
-            options[i].hidden = option[i] < 3
+            options[i].hidden = i <= 6
         }else{
             {}
         }
     }
+}
+
+function changeLevel(){
+    const options = document.getElementById('weaponBreak')
+    document.getElementById('weaponBreak').selectedIndex = 0
+
+    const weaponBElement = document.getElementById('weaponLevel')
+    const selectedClass = weaponBElement.options[weaponBElement.selectedIndex].value
+    for(let i = 1; i < options.length; i++){
+        if(i===0){
+            continue
+        }
+        switch(selectedClass){
+            case 'WL1':
+                options[i].hidden = i != 1
+                break
+            case 'WL20':
+                options[i].hidden = i != 1 || i != 2 
+                break
+            case 'WL40':
+                options[i].hidden = i != 2 || i != 3
+                break
+            case 'WL50':
+                options[i].hidden = i != 3 || i != 4
+                break
+            case 'WL60':
+                options[i].hidden = i != 4 || i != 5
+                break
+            case 'WL70':
+                options[i].hidden = i != 5 || i != 6
+            case 'WL80':
+                options[i].hidden = i != 6 || i != 7
+                break
+            case 'WL90':
+                options[i].hidden = i != 7
+                break
+        }
+    }
+
 }
