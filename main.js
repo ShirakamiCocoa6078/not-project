78d9cb1e5aa6ef9ea4bcb8240bb59434362ebb07
Hotococoa6078 <71052778+Hotococoa6078@users.noreply.github.com>
2021-03-13 12:59:08 +0900

    update

--- main.js
+++ main.js
@@ -3,9 +3,25 @@ function characterChanged() {
     document.getElementById('weapon').selectedIndex = 0
 
     const characterElement = document.getElementById('character')
-    const selectedClass = characterElement.options[characterElement.selectedIndex].text
+    const selectedClass = characterElement.options[characterElement.selectedIndex].value
     for (let i = 1; i < options.length; i++) {
         options[i].hidden = options[i].value != selectedClass
     }
 }
-characterChanged()
+
+function changeWeapon(){
+    const options1 = document.getElementById('weaponLevel')
+    const options2 = document.getElementById('weaponBreak')
+    document.getElementById('weaponLevel').selectedIndex = 0
+    document.getElementById('weaponBreak').selectedIndex = 0
+
+    const weaponElement = document.getElementById('weapon')
+    const selectedClass = weaponElement.options[weaponElement.selectedIndex].value
+    for(let i = 1; i< options1.length; i++){
+        if(selectedClass[-1].match(/1|2/)){
+            options[i].hidden = option[i] < 3
+        }else{
+            {}
+        }
+    }
+}
