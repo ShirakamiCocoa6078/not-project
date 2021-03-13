248d8fadc94bc33b8ac606c7746e63ff38d0c7c5
Hotococoa6078 <71052778+Hotococoa6078@users.noreply.github.com>
2021-03-13 17:02:22 +0900

    update

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-function characterChanged() {
+const ElementList = [
+    'characterLevel',
+    'characterBreak',
+    'weapon',
+    'weaponLevel',
+    'weaponBreak',
+    'talents1',
+    'talents2',
+    'talents3'
+]
+function characterChanged() { //캐릭터 변경시
+    const characterLevelOptions = document.getElementById('characterLevel').options
+    const characterSelectedClass = document.getElementById('character').selectedIndex
+    for(let i = 1; i < ElementList.length; i++){
+        document.getElementById(ElementList[i]).selectedIndex = 0
+    }
+    for(let i = 1; i < characterLevelOptions.length; i++){
+        if(characterSelectedClass != 0){
+            characterLevelOptions[i].hidden = false
+        }else{
+            characterLevelOptions[i].hidden = i != 0
+        }
+    }
     const options = document.getElementById('weapon').options
     document.getElementById('weapon').selectedIndex = 0
 
@@ -9,21 +31,23 @@ function characterChanged() {
     }
 }
 
-function changeWeapon(){
+function changeWeapon(){//무기 변경시
     const options = document.getElementById('weaponLevel')
     document.getElementById('weaponLevel').selectedIndex = 0
 
     const weaponElement = document.getElementById('weapon')
     const selectedClass = weaponElement.options[weaponElement.selectedIndex].value
     for(let i = 1; i< options.length; i++){
-        if(selectedClass[selectedClass.length-1].match(/1|2/)){
+        if(selectedClass[selectedClass.length-1] === '0'){
+            options[i].hidden = true
+        }else if(selectedClass[selectedClass.length-1].match(/1|2/)){
             options[i].hidden = i >= 7
         }else{
-            {}
+            options[i].hidden = false
         }
     }
 }
-function changeCLevel(){
+function changeCLevel(){//캐릭터 레벨 변경시
     const options = document.getElementById('characterBreak')
     document.getElementById('characterBreak').selectedIndex = 0
 
@@ -61,7 +85,7 @@ function changeCLevel(){
         }
     }
 }
-function changeLevel(){
+function changeLevel(){//무기 레벨 변경시
     const options = document.getElementById('weaponBreak')
     document.getElementById('weaponBreak').selectedIndex = 0
 
